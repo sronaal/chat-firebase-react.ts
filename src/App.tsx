670852a8bfefc4e8f2e3e89318d1e0955b1e964d
Ronal <ronal.sanx21@gmail.com>
2025-06-06 17:52:35 -0500

    Creacion Proyecto Instalacion Dependecias

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <Button>Iniciar Sesión</Button>
+    </>
+  )
+}
+
+export default App
