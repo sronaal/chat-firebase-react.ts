285fb94495cfa7927f16b60bb9bce96cb8be47aa
Ronal <ronal.sanx21@gmail.com>
2025-06-07 14:28:51 -0500

    Profile

--- src/components/chat/Profile.tsx
+++ src/components/chat/Profile.tsx
@@ -1,8 +1,16 @@
+import { Button } from "../ui/button"
 
 function Profile() {
   return (
-    <div className='bg-orange-100'>
-      <h1>Profile</h1>
+    <div className='p-4 text-center'>
+      <img 
+      src="https://randomuser.me/api/portraits/men/44.jpg" alt="Foto de perfil" 
+      className="rounded-md mb-4 mx-auto w-24 h-24"
+      />
+      <h1 className="text-bold text-2xl">Profile</h1>
+      <p className="font-semibold mb-2">Ronal Sánchez</p>
+      <p className="text-gray-500 mb-2">ronal@gmail.com</p>
+      <Button className="bg-black text-white w-full">Cerrar sesión</Button>
     </div>
   )
 }
